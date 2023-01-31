662cad58ea056279033bf0ceda5f65bf19bcdd7e
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-01-31 16:24:23 +0300

    Update GreatestNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+     first=1;
+     second=2;
+     if(first>=second){
+      System.out.println(first);} 
+     else{
+      System.out.println(second);} 
     }
 }
